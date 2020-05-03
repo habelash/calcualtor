b97c76ad656deedf7defbc50e3fe1541761451af
habelash <habelash@gmail.com>
2020-05-03 11:33:51 +0530

    dictionary in python

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+monthconversion = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sept": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+
+}
+print(monthconversion.get("Div","Not Defined"))
